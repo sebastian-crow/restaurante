aebf1ef5e3fe9fb14255cdef6ec67f8e91136cb0
Marin28c1997 <>
2023-02-23 17:38:31 -0500

    update

--- Pages/Admin/main.js
+++ Pages/Admin/main.js
@@ -4,16 +4,57 @@ createApp({
   data() {
     return {
       orders: "",
+      type: "",
+      description: "",
+      product: [],
+      imagenSeleccionada: "",
+      price: "",
+      name:"",
     };
   },
-  methods: {},
+  methods: {
+    agregarUsuario() {
+      if (this.price <= 0) {
+        swal("Error", "El precio no puede ser menor o igual que 0", "error");
+      } else {
+        this.product.push({
+          name:this.name,
+          type: this.type,
+          price: this.price,
+          description: this.description,
+          imagen: this.imagenSeleccionada,
+        });
+
+        localStorage.setItem("productos", JSON.stringify(this.product));
+        this.type = "";
+         this.price = "";
+        this.description = "";
+        this.imagenSeleccionada = "";
+        this.name=""
+      }
+    },
+
+    seleccionarImagenPredeterminada(evento) {
+      const archivo = evento.target.files[0];
+      const lector = new FileReader();
+      lector.onload = () => {
+        this.imagenSeleccionada = lector.result;
+      };
+      lector.readAsDataURL(archivo);
+    },
+  },
   beforeCreate() {
     /*
     funcion para cargar los datos almacenados en localStoragede de orders 
     */
     this.orders = JSON.parse(localStorage.getItem("orders"));
   },
-  mounted() {},
+  mounted() {
+    const listProduct = JSON.parse(localStorage.getItem("productos"));
+    if (listProduct !== null) {
+      this.product = listProduct;
+    }
+  },
   beforeUpdate() {},
   update() {},
 }).mount("#admin");
