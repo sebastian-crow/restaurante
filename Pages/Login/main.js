29cfea2ec763721367853cbf877c986368f7ac22
sebastian-crow <sebastianberiio@gmail.com>
2023-02-24 12:04:36 -0500

    App UI Done

--- Pages/Login/main.js
+++ Pages/Login/main.js
@@ -3,10 +3,36 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      login: "hello world",
+      input: {
+        username: "",
+        password: "",
+      },
+      users: [
+        { username: "dummychef", password: "contraseñasegura" },
+        { username: "juanhorseadmin21", password: "hola123" },
+        { username: "waiterjorge", password: "waiter123" },
+      ],
     };
   },
-  methods: {},
+  methods: {
+    login() {
+      if (this.input.username !== "" && this.input.password !== "") {
+        const user = this.users.find(
+          (user) =>
+            user.username === this.input.username &&
+            user.password === this.input.password
+        );
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user));
+          location.href = "../../index.html";
+        } else {
+          alert("El usuario no existe");
+        }
+      } else {
+        alert("Debes llenar todos los campos para poder continuar");
+      }
+    },
+  },
   beforeCreate() {},
   mounted() {},
   beforeUpdate() {},
