5fdfb4b9fa195d57eafb691640911154ae3ff9cb
juans <juansmgb@gmail.com>
2023-02-24 17:37:34 -0500

    vista del mesero y funcinalidades del adomiciliario

--- Pages/Waiter/main.js
+++ Pages/Waiter/main.js
@@ -3,12 +3,48 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      waiter: "hello world",
+      orders: "",
     };
   },
-  methods: {},
-  beforeCreate() {},
+  methods: {
+    /*
+     Cambia el estado de Pendiente a Pedido listo,
+     al hacer esto el color del boton cambia de rojo a azul
+     */
+    statusToPreparedOrder(id) {
+      let button = document.getElementById(id);
+      button.className = "btn btn-primary";
+      const orders = this.orders.map((order) =>
+        order.id === id ? (order.status = "Pedido listo") : "Pendiente"
+      );
+      localStorage.setItem("orders", JSON.stringify(orders));
+      this.orders = orders;
+    }, setTimeOrder(id) {
+      setTimeout(function(){
+          statusToPreparedOrder(id);
+      }, 900000); // 15 minutos
+  },
+  //Agregamos la funcion para cambiar el estado del pedido a entregado
+  changeStatusOrder(id,name){
+    let orders = this.orders.map((order) =>
+      order.id === id ? (order.status = "Entregado por "+name) : order.status
+    );
+    //Agregamos la hora capturada
+    let currentDate = new Date();
+    let time = currentDate.getHours() + ':' + currentDate.getMinutes();
+    orders.time = time;
+    localStorage.setItem("orders", JSON.stringify(orders));
+    this.orders = orders;
+  }
+  
+  },
+  beforeCreate() {
+    /*
+    funcion para cargar los datos almacenados en localStoragede de orders 
+    */
+    this.orders = JSON.parse(localStorage.getItem("orders"));
+  },
   mounted() {},
   beforeUpdate() {},
   update() {},
-}).mount("#waiter");
+}).mount("#mesero");
