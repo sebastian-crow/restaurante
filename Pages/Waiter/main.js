13e9cb39ce8bfbab5750a54aad5f56261756fec1
sebastian-crow <sebastianberiio@gmail.com>
2023-02-26 15:35:43 -0500

    Waiter integration 2.0

--- Pages/Waiter/main.js
+++ Pages/Waiter/main.js
@@ -12,61 +12,9 @@ createApp({
       ],
       idOrder: "",
       idDomiciliary: "",
-      // orders: [
-      //   { id: 1, cart: [{ img: 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_27/1586836/hotdogs-te-square-200702.jpg', name: 'perro', cantidad: 10, VU: 1, VT: 10 }], status: 'Pedido listo', TotalPedido: 1000 },
-      //   {
-      //     id: 2, cart: [{ img: 'https://bakeitwithlove.com/wp-content/uploads/2022/01/what-to-serve-with-burgers-sq.jpg', name: 'hamburguesa doble carne con papas', cantidad: 20, VU: 2, VT: 20 },
-      //     { img: 'https://bakeitwithlove.com/wp-content/uploads/2022/01/what-to-serve-with-burgers-sq.jpg', name: 'hamburguesa', cantidad: 20, VU: 1, VT: 20 }], status: 'Pendiente', TotalPedido: 2000
-      //   },
-      //   {
-      //     id: 3, cart: [{ img: 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_27/1586836/hotdogs-te-square-200702.jpg', name: 'perro', cantidad: 50, VU: 5, VT: 60 },
-      //     { img: 'https://bakeitwithlove.com/wp-content/uploads/2022/01/what-to-serve-with-burgers-sq.jpg', name: 'hamburguesa', cantidad: 50, VU: 6, VT: 80 },
-      //     { img: 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_27/1586836/hotdogs-te-square-200702.jpg', name: 'perro', cantidad: 50, VU: 7, VT: 90 }], status: 'Cancelado', TotalPedido: 3000
-      //   },
-      //   {
-      //     id: 4, cart: [{ img: 'https://bakeitwithlove.com/wp-content/uploads/2022/01/what-to-serve-with-burgers-sq.jpg', name: 'hamburguesa doble carne con papas', cantidad: 20, VU: 2, VT: 20 },
-      //     { img: 'https://bakeitwithlove.com/wp-content/uploads/2022/01/what-to-serve-with-burgers-sq.jpg', name: 'hamburguesa', cantidad: 20, VU: 1, VT: 20 }], status: 'Pedido listo', TotalPedido: 2000
-      //   },
-      //   {
-      //     id: 5, cart: [{ img: 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_27/1586836/hotdogs-te-square-200702.jpg', name: 'perro', cantidad: 50, VU: 5, VT: 60 },
-      //     { img: 'https://bakeitwithlove.com/wp-content/uploads/2022/01/what-to-serve-with-burgers-sq.jpg', name: 'hamburguesa 2', cantidad: 50, VU: 6, VT: 80 },
-      //     { img: 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2020_27/1586836/hotdogs-te-square-200702.jpg', name: 'perro 2', cantidad: 50, VU: 7, VT: 90 }], status: 'Despachar', TotalPedido: 3000
-      //   },
-      // ],
     };
   },
   methods: {
-    /*
-     Cambia el estado de Pendiente a Pedido listo,
-     al hacer esto el color del boton cambia de rojo a azul
-     */
-    // statusToPreparedOrder(id) {
-    //   let button = document.getElementById(id);
-    //   button.className = "btn btn-primary";
-    //   const orders = this.orders.map((order) =>
-    //     order.id === id ? (order.status = "Pedido listo") : "Pendiente"
-    //   );
-    //   localStorage.setItem("orders", JSON.stringify(orders));
-    //   this.orders = orders;
-    // },
-    // setTimeOrder(id) {
-    //   setTimeout(function () {
-    //     statusToPreparedOrder(id);
-    //   }, 900000); // 15 minutos
-    // },
-    // //Agregamos la funcion para cambiar el estado del pedido a entregado
-    // changeStatusOrder(id) {
-    //   let orders = this.orders.map((order) =>
-    //     order.id === id ? (order.status = "Entregado por " + name) : order.status
-    //   );
-    //   //Agregamos la hora capturada
-    //   let currentDate = new Date();
-    //   let time = currentDate.getHours() + ':' + currentDate.getMinutes();
-    //   orders.time = time;
-    //   localStorage.setItem("orders", JSON.stringify(orders));
-    //   this.orders = orders;
-    // },
-
     /*
     Carga los datos del domiciliario al localStorage
     */
