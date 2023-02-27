e0f4f68dc6765adab19fced411b65029a8d2d481
sebastian-crow <sebastianberiio@gmail.com>
2023-02-27 10:48:08 -0500

    waiter

--- Pages/Waiter/main.js
+++ Pages/Waiter/main.js
@@ -171,37 +171,3 @@ createApp({
   beforeUpdate() {},
   update() {},
 }).mount("#mesero");
-
-/* 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Mesero</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-      crossorigin="anonymous"
-    />
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-  </head>
-
-  <body>
-    <div id="mesero"></div>
-
-    <!-- FIN MODAL -->
-
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-      crossorigin="anonymous"
-    ></script>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="main.js"></script>
-  </body>
-</html>
-
-*/
