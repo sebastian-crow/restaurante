34a183c919ef89fc2fc9e61cea2f38e9de45245c
sebastian-crow <sebastianberiio@gmail.com>
2023-02-27 12:22:45 -0500

    Bug images fixed

--- Pages/Menu/main.js
+++ Pages/Menu/main.js
@@ -3,95 +3,6 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      products: [
-        {
-          name: "HotDogs",
-          content: [
-            {
-              id: 1,
-              img: "https://playswellwithbutter.com/wp-content/uploads/2022/05/Grilled-Hot-Dogs-How-to-Grill-Hot-Dogs-38.jpg",
-              name: "HotDog Sencillo",
-              descriptionSm: "Hot Dog sencill0 con ketchup",
-              descriptionLg: "Very delicious",
-              price: 10000,
-              cant: 1,
-            },
-            {
-              id: 2,
-              img: "https://www.vvsupremo.com/wp-content/uploads/2016/02/900X570_Mexican-Style-Hot-Dogs.jpg",
-              name: "HotDog Mediano",
-              descriptionSm: "Hot Dog Mediano con salsa de tamarindo",
-              descriptionLg: "hot dog sencillo muy rica",
-              price: 20000,
-              cant: 1,
-            },
-            {
-              id: 3,
-              img: "https://i.blogs.es/c28af1/hot_dog-min/1366_2000.jpg",
-              name: "HotDog Jumbo",
-              descriptionSm: "Hot Dog sencill0",
-              descriptionLg: "hot dog sencillo muy rico",
-              price: 30000,
-              cant: 1,
-            },
-            {
-              id: 4,
-              img: "https://www.mommyhatescooking.com/wp-content/uploads/2015/05/bbq-beef-hot-dog-2.jpg",
-              name: "HotDog DobleCarne",
-              descriptionSm:
-                "Hot Dog con carne, salsa amarilla y algo que parece cebolla",
-              descriptionLg: "Hot dog con carne, delicious",
-              price: 40000,
-              cant: 1,
-            },
-          ],
-        },
-        {
-          name: "hamburger",
-          content: [
-            {
-              id: 1,
-              img: "https://mui.kitchen/__export/1617556046050/sites/muikitchen/img/2021/04/04/burger-868145_1280.jpg_1714320005.jpg",
-              name: "Hamburger Sencilla",
-              descriptionSm:
-                "Hamburguesa sencilla con carne, queso cheddar y tocineta",
-              descriptionLg: "hamburguesa sencilla muy rica",
-              price: 10000,
-              cant: 1,
-            },
-            {
-              id: 2,
-              img: "https://www.infobae.com/new-resizer/xHORBTTOvi76_TX7OOanBUblR-0=/1200x900/filters:format(webp):quality(85)//arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/FJKXKQKMMJBV7KQ7XQ3YNFO7LU.jpg",
-              name: "Hamburger Mediana",
-              descriptionSm:
-                "Hamburguesa sencilla con carne, queso cheddar y tocineta",
-              descriptionLg: "hamburguesa sencilla muy rica",
-              price: 20000,
-              cant: 1,
-            },
-            {
-              id: 3,
-              img: "https://cdn2.cocinadelirante.com/sites/default/files/images/2020/11/receta-de-carne-para-hamburguesas-jugosas-caseras.jpg",
-              name: "Hamburger Jumbo",
-              descriptionSm:
-                "Hamburguesa sencilla con carne, queso cheddar y tocineta",
-              descriptionLg: "hamburguesa sencilla muy rica",
-              price: 30000,
-              cant: 1,
-            },
-            {
-              id: 4,
-              img: "https://www.minervafoods.com/wp-content/uploads/2022/12/como_fazer_hamburguer_caseiro_1-1.jpg",
-              name: "Hamburger DobleCarne",
-              descriptionSm:
-                "Hamburguesa sencilla con carne, queso cheddar y tocineta",
-              descriptionLg: "hamburguesa sencilla muy rica",
-              price: 40000,
-              cant: 1,
-            },
-          ],
-        },
-      ],
       cart: [],
       isLogged: false,
       productDetails: null,
@@ -197,8 +108,7 @@ createApp({
   },
   beforeCreate() {
     this.cart = JSON.parse(localStorage.getItem("cart"));
-    const products = JSON.parse(localStorage.getItem("products"));
-    this.products = products ? products : this.products;
+    this.products = JSON.parse(localStorage.getItem("products"));
   },
   mounted() {
     localStorage.setItem("products", JSON.stringify(this.products));
